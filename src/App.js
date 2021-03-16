f579a58aa3468e01de567dac8de068ac5f2a13db
Đinh Sỹ Duyệt kênh <duyetsydinh@gmail.com>
2021-03-16 10:58:35 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+  
+  
+ } from "react-router-dom";
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Header from './components/Header';
+import News from './News';
+import './App.css'
+
+
+class App extends React.Component {
+   render() {
+      return (
+         <Router>
+      <div>
+         <Header>
+      
+       </Header>
+       <div className="content">
+        <Switch>
+        <Route path="/News">
+        <News/>
+        </Route>
+
+          <Route path="/About">
+        
+            <About />
+           
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </div>
+      </div>
+    </Router>
+      )
+   }
 }
-
 export default App;
